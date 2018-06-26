73b986b27e33d8f2aef715cf671b7886dbf665bd
Pandazki <onlrrr@gmail.com>
2018-06-26 20:23:11 +0800

    revert main.js

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
 const Pomelo = require('./lib/Pomelo');
 
 module.exports = new Pomelo({
-  wsCreator({
-    url,
-    onError,
-    onOpen,
-    onMessage,
-    onClose
-  }) {
-    const ws = wx.connectSocket({
-      url: url
-    });
-    ws.onError(onError);
-    ws.onOpen(onOpen);
-    ws.onMessage(onMessage);
-    ws.onClose(onClose);
-    return ws;
-  },
-  urlGenerator(host, port) {
-    if (host === 'localhost' || host === '127.0.0.1') {
-      return 'ws://' + host + ':' + port
-    } else {
-      let url = 'wss://' + host;
-      if (port) {
-        url += '/ws/' + port + '/';
-      }
-      return url;
+    wsCreator({url, onError, onOpen, onMessage, onClose}) {
+        const ws = wx.connectSocket({url:url});
+        ws.onError(onError);
+        ws.onOpen(onOpen);
+        ws.onMessage(onMessage);
+        ws.onClose(onClose);
+        return ws;
+    },
+    urlGenerator(host, port) {
+        let url = 'wss://' + host;
+        if (port) {
+            url += '/ws/' + port + '/';
+        }
+        return url;
     }
-  }
 });
